f3308c11c9577e8e2b07777fca1dbba386c0c19f
loginloop <trungnghia260114@gmail.com>
2019-10-08 10:19:30 +0700

    add space

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,10 +2,12 @@ export const addProductToCart = product => ({
   type: 'ADD_PRODUCT_TO_CART',
   product
 })
+
 export const removeCartItem = idRemove => ({
   type: 'REMOVE_CART_ITEM',
   idRemove
 })
+
 export const setQuantityCartItem = (idSet, quantity) => ({
   type: 'SET_QUANTITY_CART_ITEM',
   idSet,
